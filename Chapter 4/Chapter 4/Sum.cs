22bbec8c4210f13c5d3caf7b9c86a6cac084096d
smoore8 <savionmoore8@gmail.com>
2014-03-25 10:10:38 -0400

    Finish Tommorow

---
+++ Chapter 4/Chapter 4/Sum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter_4
+{
+    class Sum
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int n;
+            String input;
+            {
+                while (true)
+                {
+                    Console.WriteLine("Enter any interger value");
+                    input = Console.ReadLine();
+                    n = Convert.ToInt32(input);
+
+                    if (input == "999")
+                    {
+                        break;
+
+
+                }
+                    sum = n + sum;
+
+            }
+
+        }
+    }
+}
